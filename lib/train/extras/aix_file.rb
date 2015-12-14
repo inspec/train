8f8f93765d8cb3db1e3bb6b4b0ad2cf751df5c56
Jeremy W. Chalfant <jchalfan@us.ibm.com>
2015-12-14 16:13:54 -0600

    cleanup and all tests pass

--- lib/train/extras/aix_file.rb
+++ lib/train/extras/aix_file.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 # author: Dominik Richter
 # author: Christoph Hartmann
+# author: Jeremy Chalfant
 
 require 'shellwords'
 require 'train/extras/stat'
@@ -14,17 +15,10 @@ module Train::Extras
 
     def content
       return @content if defined?(@content)
-      @content = @backend.run_command(
-        "cat #{@spath} || echo -n").stdout
+      @content = @backend.run_command("cat #{@spath}").stdout || ''
       return @content unless @content.empty?
       @content = nil if directory? or size.nil? or size > 0
       @content
-    end
-
-    def link_target
-      return @link_target if defined? @link_target
-      return @link_target = nil if link_path.nil?
-      @link_target = @backend.file(link_path)
     end
 
     def link_path
@@ -34,21 +28,6 @@ module Train::Extras
           "perl -e '$ln = readlink(shift) or exit 2; print $ln' #{@spath}"
         ).stdout
       )
-    end
-
-    def mounted?
-      @mounted ||= (
-        !@backend.run_command("mount | grep -- ' on #{@spath}'")
-                 .stdout.empty?
-      )
-    end
-
-    def product_version
-      nil
-    end
-
-    def file_version
-      nil
     end
   end
 end
