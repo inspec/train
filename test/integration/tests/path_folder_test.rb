0e660267f3f91d867ae1e55b66f5e2ec18350418
Jeremy W. Chalfant <jchalfan@us.ibm.com>
2015-12-14 15:50:30 -0600

    AIX+SSH working path_folder_test

--- test/integration/tests/path_folder_test.rb
+++ test/integration/tests/path_folder_test.rb
@@ -18,7 +18,8 @@ describe 'file interface' do
       file.type.must_equal(:directory)
     end
 
-    if get_backend.call.os[:family] == 'freebsd'
+    case get_backend.call.os[:family]
+    when 'freebsd'
       it 'has freebsd folder content behavior' do
         file.content.must_equal("\u0003\u0000")
       end
@@ -30,6 +31,20 @@ describe 'file interface' do
       it 'has an sha256sum' do
         file.sha256sum.must_equal('9b4fb24edd6d1d8830e272398263cdbf026b97392cc35387b991dc0248a628f9')
       end
+
+    when 'aix'
+      it 'has freebsd folder content behavior' do
+        file.content.must_equal("p\a.\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0002..\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000")
+      end
+
+      it 'has an md5sum' do
+        file.md5sum.must_equal('4c8c0c3807e906c23261d19e5f535cd2')
+      end
+
+      it 'has an sha256sum' do
+        file.sha256sum.must_equal('cac0ea0bb8ecc4561009e9480e73313e5b36a27005e685d88227f4d66abbb0ea')
+      end
+
     else
       it 'has no content' do
         file.content.must_equal(nil)
