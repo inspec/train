d9f29bc3f26c3a2cd4b58fb00d7aeabe50e14cc8
Jere Julian <jere@arista.com>
2016-11-02 10:01:13 -0400

    Include support for the network device CLI and bash shell
    Signed-off-by: Jere Julian <jere@arista.com>

--- lib/train/extras/os_detect_arista_eos.rb
+++ lib/train/extras/os_detect_arista_eos.rb
@@ -1,23 +1,28 @@
 # encoding: utf-8
-# author: Dominik Richter
-# author: Christoph Hartmann
+# author: Jere Julian
 #
-# This is heavily based on:
-#
-#   OHAI https://github.com/chef/ohai
-#   by Adam Jacob, Chef Software Inc
-#
+# Arista EOS has 2 modes. Most compliance tests will use the network CLI
+# but when working with vagrant, its common to encounter the raw bash shell.
 require 'json'
 
 module Train::Extras
-  module DetectEos
-    def detect_eos
-      output = @backend.run_command('show version | json').stdout
-      if output
+  module DetectAristaEos
+    def detect_arista_eos
+      if unix_file?('/usr/bin/FastCli')
+        output = @backend.run_command('FastCli -p 15 -c "show version | json"').stdout
+        @platform[:name] = 'arista_eos_bash'
+        family = 'fedora'
+      else
+        output = @backend.run_command('show version | json').stdout
+      end
+
+      unless output.empty?
         eos_ver = JSON.parse(output)
-        @platform[:family] = 'eos'
-        @platform[:name] = 'eos'
+        @platform[:name] = @platform[:name] || 'arista_eos'
+        family ||= 'arista_eos'
+        @platform[:family] = family
         @platform[:release] = eos_ver['version']
+        @platform[:arch] = eos_ver['architecture']
         true
       end
     end
