a3cd8db6678eb981a419a074c75d08adf9fa78a8
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-01-30 11:57:26 -0500

    Linting issues
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/train/platforms/detect/specifications/api.rb
+++ lib/train/platforms/detect/specifications/api.rb
@@ -1,12 +1,4 @@
-
 # encoding: utf-8
-
-# rubocop:disable Style/Next
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/ClassLength
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/PerceivedComplexity
 
 module Train::Platforms::Detect::Specifications
   class Api
--- lib/train/transports/aws.rb
+++ lib/train/transports/aws.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'train/plugins'
 require 'aws-sdk'
 
