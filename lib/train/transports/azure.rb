071ce59ed88a3a9a07f624bcd17b92f11e7fc077
Ruairi Fennell <rfennell@chef.io>
2018-09-19 11:17:55 +0100

    Adds Azure Vault Client, optional caching
    Signed-off-by: Ruairi Fennell <rfennell@chef.io>

--- lib/train/transports/azure.rb
+++ lib/train/transports/azure.rb
@@ -4,10 +4,12 @@ require 'train/plugins'
 require 'ms_rest_azure'
 require 'azure_mgmt_resources'
 require 'azure_graph_rbac'
+require 'azure_mgmt_key_vault'
 require 'socket'
 require 'timeout'
 require 'train/transports/helpers/azure/file_credentials'
 require 'train/transports/clients/azure/graph_rbac'
+require 'train/transports/clients/azure/vault'
 
 module Train::Transports
   class Azure < Train.plugin(1)
@@ -59,15 +61,17 @@ module Train::Transports
         force_platform!('azure', @platform_details)
       end
 
-      def azure_client(klass = ::Azure::Resources::Profiles::Latest::Mgmt::Client)
-        if cache_enabled?(:api_call)
+      def azure_client(klass = ::Azure::Resources::Profiles::Latest::Mgmt::Client, use_cache = true, vault_name = nil)
+        if use_cache && cache_enabled?(:api_call)
           return @cache[:api_call][klass.to_s.to_sym] unless @cache[:api_call][klass.to_s.to_sym].nil?
         end
 
         if klass == ::Azure::Resources::Profiles::Latest::Mgmt::Client
           @credentials[:base_url] = MsRestAzure::AzureEnvironments::AzureCloud.resource_manager_endpoint_url
         elsif klass == ::Azure::GraphRbac::Profiles::Latest::Client
-          client =  GraphRbac.client(@credentials)
+          client = GraphRbac.new.client(@credentials)
+        elsif klass == ::Azure::KeyVault::Profiles::Latest::Mgmt::Client
+          client = Vault.new(vault_name).client(@credentials)
         end
 
         client ||= klass.new(@credentials)
--- lib/train/transports/clients/azure/vault.rb
+++ lib/train/transports/clients/azure/vault.rb
@@ -1,14 +1,18 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require 'azure_graph_rbac'
+require 'azure_mgmt_key_vault'
 
-# Wrapper class for ::Azure::GraphRbac::Profiles::Latest::Client allowing custom configuration,
+# Wrapper class for ::Azure::KeyVault::Profiles::Latest::Mgmt::Client allowing custom configuration,
 # for example, defining additional settings for the ::MsRestAzure::ApplicationTokenProvider.
-class GraphRbac
-  def initialize
+class Vault
+  def initialize(vault_name)
+    if vault_name.nil?
+      raise ::Train::UserError, 'Vault Name cannot be nil'
+    end
     @auth_endpoint = MsRestAzure::AzureEnvironments::AzureCloud.active_directory_endpoint_url
-    @api_endpoint = MsRestAzure::AzureEnvironments::AzureCloud.active_directory_graph_resource_id
+    @api_endpoint = "https://#{vault_name}#{MsRestAzure::AzureEnvironments::AzureCloud.key_vault_dns_suffix}"
+    @resource_endpoint = 'https://vault.azure.net'
   end
 
   def client(credentials)
@@ -20,13 +24,13 @@ class GraphRbac
     )
     credentials[:credentials] = ::MsRest::TokenCredentials.new(provider)
     credentials[:base_url] = @api_endpoint
-    ::Azure::GraphRbac::Profiles::Latest::Client.new(credentials)
+    ::Azure::KeyVault::Profiles::Latest::Mgmt::Client.new(credentials)
   end
 
   def settings
     client_settings = MsRestAzure::ActiveDirectoryServiceSettings.get_azure_settings
     client_settings.authentication_endpoint = @auth_endpoint
-    client_settings.token_audience = @api_endpoint
+    client_settings.token_audience = @resource_endpoint
     client_settings
   end
 end
