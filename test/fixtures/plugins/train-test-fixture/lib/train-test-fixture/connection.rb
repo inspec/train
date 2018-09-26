7d9e779c4cc877d052ef82bf2a17a4787c59d6d8
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-09-26 12:11:43 -0400

    train-test-fixture feature complete
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/fixtures/plugins/train-test-fixture/lib/train-test-fixture/connection.rb
+++ test/fixtures/plugins/train-test-fixture/lib/train-test-fixture/connection.rb
@@ -1,4 +1,5 @@
 require 'train-test-fixture/platform'
+require 'train/transports/local'
 
 module TrainPlugins
   module TestFixture
@@ -9,6 +10,30 @@ module TrainPlugins
         super(options)
       end
 
+      def local?
+        true
+      end
+
+      private
+
+      def run_command_via_connection(cmd)
+        Train::Transports::Local::CommandResult.new(
+          'Mock Command Result stdout',
+          'Mock Command Result stderr',
+          17
+        )
+      end
+
+      def file_via_connection(path, *args)
+        MockFile.new(self, path)
+      end
+
+      class MockFile < Train::File
+        def content
+          # Remarkably, the content is always the same.
+          'Lorem Ipsum'
+        end
+      end
     end
   end
 end
