31ef7c65f07975bb67f3a5f644f1ed8d871ba35a
Jerry Aldrich <jerryaldrichiii@gmail.com>
2018-10-12 14:00:25 -0700

    Respond to feedback
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

--- lib/train/platforms/detect/specifications/os.rb
+++ lib/train/platforms/detect/specifications/os.rb
@@ -21,10 +21,8 @@ module Train::Platforms::Detect::Specifications
             is_windows = false
             is_windows = true if winrm?
 
-            if defined?(Train::Transports::Local::Connection)
-              if @backend.class == Train::Transports::Local::Connection
-                is_windows = true
-              end
+            if @backend.class.to_s == 'Train::Transports::Local::Connection'
+              is_windows = true if ruby_host_os(/mswin|mingw32|windows/)
             end
 
             is_windows
