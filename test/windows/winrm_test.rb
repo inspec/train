2f8c54716d7dd276bfa2e12c40879f8f6b04203b
Miah Johnson <miah@chia-pet.org>
2019-04-29 08:56:24 -0700

    The Windows release also changed.. (#443)
    * The Windows release also changed..
    
    Signed-off-by: Miah Johnson <miah@chia-pet.org>
    
    * The Appveyor Strikes Back
    
    Or really, I learn to grep for string reuse across tests.
    
    Signed-off-by: Miah Johnson <miah@chia-pet.org>
    
    * Try using a regexp
    
    Signed-off-by: Miah Johnson <miah@chia-pet.org>
    
    * Use must_match to match against regular expressions.
    
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- test/windows/winrm_test.rb
+++ test/windows/winrm_test.rb
@@ -27,9 +27,9 @@ describe 'windows winrm command' do
 
   it 'verify os' do
     os = conn.os
-    os[:name].must_equal 'windows_server_2012_r2_datacenter'
+    os[:name].must_match(/windows_server.*_datacenter/)
     os[:family].must_equal 'windows'
-    os[:release].must_equal '6.3.9600'
+    os[:release].must_match(/\d+(\.\d+)+/)
     os[:arch].must_equal 'x86_64'
   end
 
