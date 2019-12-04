bb8599ec41a6c4772199f37bbec7619aa9bc6043
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-04 15:40:18 -0800

    Adding bundle artifact caching to BK.
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-set -ueo pipefail
+echo "--- dependencies"
+. .expeditor/buildkite/cache_support.sh
+install_cache_deps
 
 echo "--- system details"
 uname -a
 ruby -v
 bundle --version
 
+echo "--- pull bundle cache"
+pull_bundle
+
 echo "--- bundle install"
+bundle config --local path vendor/bundle
 bundle install --jobs=7 --retry=3 --without tools integration
+
+echo "--- push bundle cache"
+push_bundle
 
 echo "+++ bundle exec rake"
 bundle exec rake
