e6d0ccc83864a7fa24c4a236c93f5f5492d13f42
Ryan Davis <ryand-ruby@zenspider.com>
2020-01-22 12:59:53 -0800

    Code cleanup of os.rb & os_common.rb
    Removed needless comments on already self-documenting blocks.
    Refactored brocade_version.
    Extracted redhatish version
    Extracted local_windows?
    Logical rewrites of overly complex logic.
    Lots of `unless x.nil` -> `if !x.nil?` -> `if x`.
    Switched a number of unused unix_file_contents to unix_file_exist? calls.
    
       38.00: flay total
       616.3: flog total
        38.5: flog/method average
    
    Via walk-through with @miah.
    
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- lib/train/platforms/detect/specifications/os.rb
+++ lib/train/platforms/detect/specifications/os.rb
@@ -14,7 +14,6 @@ module Train::Platforms::Detect::Specifications
     end
 
     def self.load
-      # master family
       plat.family("os").detect { true }
 
       load_windows
@@ -25,36 +24,26 @@ module Train::Platforms::Detect::Specifications
     def self.load_windows
       plat.family("windows").in_family("os")
         .detect do
-          # Can't return from a `proc` thus the `is_windows` shenanigans
-          is_windows = false
-          is_windows = true if winrm?
-
-          if @backend.class.to_s == "Train::Transports::Local::Connection"
-            is_windows = true if ruby_host_os(/mswin|mingw32|windows/)
-          end
-
-          # Try to detect windows even for ssh transport
-          if !is_windows && detect_windows == true
-            is_windows = true
-          end
-
-          is_windows
+          (winrm? ||
+           local_windows? ||
+           detect_windows) # ssh
         end
 
-      # windows platform
       plat.name("windows").in_family("windows")
         .detect do
-          true if detect_windows == true
+          detect_windows
         end
     end
 
     def self.load_unix
-      # unix master family
       plat.family("unix").in_family("os")
         .detect do
           # we want to catch a special case here where cisco commands
           # don't return an exit status and still print to stdout
-          if unix_uname_s =~ /./ && !unix_uname_s.start_with?("Line has invalid autocommand ") && !unix_uname_s.start_with?("The command you have entered")
+          uname = unix_uname_s
+          unless (uname.empty? ||
+                  uname.start_with?("Line has invalid autocommand") ||
+                  uname.start_with?("The command you have entered"))
             @platform[:arch] = unix_uname_m
             true
           end
@@ -66,16 +55,14 @@ module Train::Platforms::Detect::Specifications
     end
 
     def self.load_linux
-      # linux master family
       plat.family("linux").in_family("unix")
         .detect do
-          true if unix_uname_s =~ /linux/i
+          unix_uname_s =~ /linux/i
         end
 
-      # debian family
       plat.family("debian").in_family("linux")
         .detect do
-          true unless unix_file_contents("/etc/debian_version").nil?
+          unix_file_exist?("/etc/debian_version")
         end
 
       register_lsb("ubuntu", "Ubuntu Linux", "debian", /ubuntu/i)
@@ -96,76 +83,66 @@ module Train::Platforms::Detect::Specifications
           if (linux_os_release && linux_os_release["NAME"] =~ /raspbian/i) || \
               unix_file_exist?("/usr/bin/raspi-config")
             @platform[:release] = unix_file_contents("/etc/debian_version").chomp
+
             true
           end
         end
+
       plat.name("debian").title("Debian Linux").in_family("debian")
         .detect do
-          lsb = read_linux_lsb
-          if lsb && lsb[:id] =~ /debian/i
-            @platform[:release] = lsb[:release]
-            true
-          end
-
           # if we get this far we have to be some type of debian
           @platform[:release] = unix_file_contents("/etc/debian_version").chomp
+
           true
         end
 
-      # fedora family
       plat.family("fedora").in_family("linux")
         .detect do
-          true if linux_os_release && linux_os_release["NAME"] =~ /fedora/i
+          rel = linux_os_release
+          rel && rel["NAME"] =~ /fedora/i
         end
+
       plat.name("fedora").title("Fedora").in_family("fedora")
         .detect do
           @platform[:release] = linux_os_release["VERSION_ID"]
           true
         end
 
-      # arista_eos family
-      # this checks for the arista bash shell
       # must come before redhat as it uses fedora under the hood
       plat.family("arista_eos").title("Arista EOS Family").in_family("linux")
         .detect do
           true
         end
+
       plat.name("arista_eos_bash").title("Arista EOS Bash Shell").in_family("arista_eos")
         .detect do
+          # this checks for the arista bash shell
           if unix_file_exist?("/usr/bin/FastCli")
             json_cmd('FastCli -p 15 -c "show version | json"')
           end
         end
 
-      # redhat family
       plat.family("redhat").in_family("linux")
         .detect do
-          # I am not sure this returns true for all redhats in this family
-          # for now we are going to just try each platform
-          # return true unless unix_file_contents('/etc/redhat-release').nil?
-
           true
         end
+
       plat.name("centos").title("Centos Linux").in_family("redhat")
         .detect do
           lsb = read_linux_lsb
           if lsb && lsb[:id] =~ /centos/i
             @platform[:release] = lsb[:release]
             true
-          elsif linux_os_release && linux_os_release["NAME"] =~ /centos/i
+          elsif (rel = linux_os_release) && rel["NAME"] =~ /centos/i
             @platform[:release] = redhatish_version(unix_file_contents("/etc/redhat-release"))
             true
           end
         end
+
       plat.name("oracle").title("Oracle Linux").in_family("redhat")
         .detect do
-          if !(raw = unix_file_contents("/etc/oracle-release")).nil?
-            @platform[:release] = redhatish_version(raw)
-            true
-          elsif !(raw = unix_file_contents("/etc/enterprise-release")).nil?
-            @platform[:release] = redhatish_version(raw)
-            true
-          end
+          (redhatish("/etc/oracle-release") ||
+           redhatish("/etc/enterprise-release"))
         end
 
       register_lsb("scientific", "Scientific Linux", "redhat", /scientific/i)
@@ -174,33 +151,36 @@ module Train::Platforms::Detect::Specifications
 
       plat.name("parallels-release").title("Parallels Linux").in_family("redhat")
         .detect do
-          unless (raw = unix_file_contents("/etc/parallels-release")).nil?
+          if (raw = unix_file_contents("/etc/parallels-release"))
             @platform[:name] = redhatish_platform(raw)
             @platform[:release] = raw[/(\d\.\d\.\d)/, 1]
             true
           end
         end
+
       plat.name("wrlinux").title("Wind River Linux").in_family("redhat")
         .detect do
-          if linux_os_release && linux_os_release["ID_LIKE"] =~ /wrlinux/i
-            @platform[:release] = linux_os_release["VERSION"]
+          rel = linux_os_release
+          if rel && rel["ID_LIKE"] =~ /wrlinux/i
+            @platform[:release] = rel["VERSION"]
             true
           end
         end
 
       register_lsb_or_content("amazon", "Amazon Linux", "redhat", "/etc/system-release", /amazon/i)
+
       register_lsb_or_content("cloudlinux", "CloudLinux", "redhat", "/etc/redhat-release", /cloudlinux/i)
 
       # keep redhat at the end as a fallback for anything with a redhat-release
       register_lsb_or_content("redhat", "Red Hat Linux", "redhat", "/etc/redhat-release", /redhat/i, /./)
 
-      # suse family
       plat.family("suse").in_family("linux")
         .detect do
-          if linux_os_release && linux_os_release["ID_LIKE"] =~ /suse/i
-            @platform[:release] = linux_os_release["VERSION"]
+          rel = linux_os_release
+          if rel && rel["ID_LIKE"] =~ /suse/i
+            @platform[:release] = rel["VERSION"]
             true
-          elsif !(suse = unix_file_contents("/etc/SuSE-release")).nil?
+          elsif (suse = unix_file_contents("/etc/SuSE-release"))
             # https://rubular.com/r/UKaYWolCYFMfp1
             version = suse.scan(/VERSION = (\d+)\nPATCHLEVEL = (\d+)/).flatten.join(".")
             # https://rubular.com/r/b5PN3hZDxa5amV
@@ -210,10 +190,9 @@ module Train::Platforms::Detect::Specifications
           end
         end
 
-      register_os_or_file("opensuse", "OpenSUSE Linux", "suse",
-                          "/etc/SuSE-release", /^opensuse/i)
-      register_os_or_file("suse",     "Suse Linux",     "suse",
-                          "/etc/SuSE-release", /^sles/i, /suse/i)
+      register_os_or_file("opensuse", "OpenSUSE Linux", "suse", "/etc/SuSE-release", /^opensuse/i)
+
+      register_os_or_file("suse",     "Suse Linux",     "suse", "/etc/SuSE-release", /^sles/i, /suse/i)
 
       register_path_and_uname("arch", "Arch Linux", "linux", "/etc/arch-release")
 
@@ -223,19 +202,18 @@ module Train::Platforms::Detect::Specifications
 
       register_path_and_uname("exherbo", "Exherbo Linux", "linux", "/etc/exherbo-release")
 
-      # alpine
+      # TODO: try using register_path
       plat.name("alpine").title("Alpine Linux").in_family("linux")
         .detect do
-          unless (raw = unix_file_contents("/etc/alpine-release")).nil?
+          if (raw = unix_file_contents("/etc/alpine-release"))
             @platform[:release] = raw.strip
             true
           end
         end
 
-      # coreos
       plat.name("coreos").title("CoreOS Linux").in_family("linux")
         .detect do
-          unless unix_file_contents("/etc/coreos/update.conf").nil?
+          if unix_file_exist?("/etc/coreos/update.conf")
             lsb = read_linux_lsb
             @platform[:release] = lsb[:release]
             true
@@ -275,10 +253,9 @@ module Train::Platforms::Detect::Specifications
       # happens when logging in as root
       plat.family("brocade").title("Brocade Family").in_family("linux")
         .detect do
-          !brocade_version.nil?
+          brocade_version
         end
 
-      # generic linux
       # this should always be last in the linux family list
       plat.name("linux").title("Generic Linux").in_family("linux")
         .detect do
@@ -286,24 +263,13 @@ module Train::Platforms::Detect::Specifications
         end
     end
 
-    def self.register_path(name, title, family, path, regexp)
-      plat.name(name).title(title).in_family(family)
-        .detect do
-          rel = unix_file_contents(path)
-          if m = regexp.match(rel)
-            @platform[:release] = m[1]
-            true
-          end
-        end
-    end
-
     def self.load_other_unix
-      # openvms
       plat.name("openvms").title("OpenVMS").in_family("unix")
         .detect do
           if unix_uname_s =~ /unrecognized command verb/i
             cmd = @backend.run_command("show system/noprocess")
-            unless cmd.exit_status != 0 || cmd.stdout.empty?
+
+            if cmd.exit_status == 0 && !cmd.stdout.empty?
               @platform[:name] = cmd.stdout.downcase.split(" ")[0]
               cmd = @backend.run_command('write sys$output f$getsyi("VERSION")')
               @platform[:release] = cmd.stdout.downcase.split("\n")[1][1..-1]
@@ -314,42 +280,31 @@ module Train::Platforms::Detect::Specifications
           end
         end
 
-      # aix
       plat.family("aix").in_family("unix")
         .detect do
-          true if unix_uname_s =~ /aix/i
+          unix_uname_s =~ /aix/i
         end
+
       plat.name("aix").title("Aix").in_family("aix")
         .detect do
           out = @backend.run_command("uname -rvp").stdout
-          m = out.match(/(\d+)\s+(\d+)\s+(.*)/)
-          unless m.nil?
-            @platform[:release] = "#{m[2]}.#{m[1]}"
-            @platform[:arch] = m[3].to_s
+          if out =~ /(\d+)\s+(\d+)\s+(.*)/
+            @platform[:release] = "#{$2}.#{$1}"
+            @platform[:arch] = "#{$3}"
           end
           true
         end
 
-      # solaris family
       plat.family("solaris").in_family("unix")
         .detect do
           if unix_uname_s =~ /sunos/i
-            unless (version = /^5\.(?<release>\d+)$/.match(unix_uname_r)).nil?
-              @platform[:release] = version["release"]
-            end
+            @platform[:release] = $1 if unix_uname_r =~ /^5\.(\d+)$/
 
             arch = @backend.run_command("uname -p")
             @platform[:arch] = arch.stdout.chomp if arch.exit_status == 0
             true
           end
         end
-
-      def self.register_path_regexp(name, title, family, path, regexp)
-        plat.name(name).title(title).in_family(family)
-          .detect do
-            regexp =~ unix_file_contents(path)
-          end
-      end
 
       # TODO: these regexps are probably needlessly wasteful
       register_path_regexp("smartos", "SmartOS", "solaris", "/etc/release", /^.*(SmartOS).*$/)
@@ -365,11 +320,10 @@ module Train::Platforms::Detect::Specifications
       plat.name("solaris").title("Solaris").in_family("solaris")
         .detect do
           rel = unix_file_contents("/etc/release")
-          if !(m = /Oracle Solaris (\d+)/.match(rel)).nil?
-            # TODO: should be string!
-            @platform[:release] = m[1]
+          if rel =~ /Oracle Solaris (\d+)/
+            @platform[:release] = $1
             true
-          elsif /^\s*(Solaris)\s.*$/ =~ rel
+          elsif rel =~ /^\s*(Solaris)\s.*$/
             true
           end
 
@@ -377,24 +331,26 @@ module Train::Platforms::Detect::Specifications
           true
         end
 
-      # hpux
       plat.family("hpux").in_family("unix")
         .detect do
-          true if unix_uname_s =~ /hp-ux/i
+          unix_uname_s =~ /hp-ux/i
         end
+
       plat.name("hpux").title("Hpux").in_family("hpux")
         .detect do
           @platform[:release] = unix_uname_r.lines[0].chomp
+          # TODO: no tests
           true
         end
 
-      # qnx
       plat.family("qnx").in_family("unix")
         .detect do
-          true if unix_uname_s =~ /qnx/i
+          unix_uname_s =~ /qnx/i
         end
+
       plat.name("qnx").title("QNX").in_family("qnx")
         .detect do
+          # TODO: refactor these uname patterns
           @platform[:name] = unix_uname_s.lines[0].chomp.downcase
           @platform[:release] = unix_uname_r.lines[0].chomp
           @platform[:arch] = unix_uname_m
@@ -403,34 +359,36 @@ module Train::Platforms::Detect::Specifications
     end
 
     def self.load_bsd
-      # bsd family
       plat.family("bsd").in_family("unix")
         .detect do
           # we need a better way to determine this family
           # for now we are going to just try each platform
           true
         end
+
       plat.family("darwin").in_family("bsd")
         .detect do
           if unix_uname_s =~ /darwin/i
             cmd = unix_file_contents("/usr/bin/sw_vers")
-            unless cmd.nil?
-              m = cmd.match(/^ProductVersion:\s+(.+)$/)
-              @platform[:release] = m.nil? ? nil : m[1]
-              m = cmd.match(/^BuildVersion:\s+(.+)$/)
-              @platform[:build] = m.nil? ? nil : m[1]
+
+            if cmd
+              @platform[:release] = cmd[/^ProductVersion:\s+(.+)$/, 1]
+              @platform[:build]   = cmd[/^BuildVersion:\s+(.+)$/, 1]
             end
-            @platform[:release] = unix_uname_r.lines[0].chomp if @platform[:release].nil?
-            @platform[:arch] = unix_uname_m
+
+            @platform[:release] ||= unix_uname_r.lines[0].chomp
+            @platform[:arch]      = unix_uname_m
             true
           end
         end
+
       plat.name("mac_os_x").title("macOS X").in_family("darwin")
         .detect do
           cmd = unix_file_contents("/System/Library/CoreServices/SystemVersion.plist")
           @platform[:uuid_command] = "system_profiler SPHardwareDataType | awk '/UUID/ { print $3; }'"
-          true if cmd =~ /Mac OS X/i
+          cmd =~ /Mac OS X/i
         end
+
       plat.name("darwin").title("Darwin").in_family("darwin")
         .detect do
           # must be some other type of darwin
@@ -444,40 +402,38 @@ module Train::Platforms::Detect::Specifications
     end
 
     def self.load_other
-      # arista_eos family
       plat.family("arista_eos").title("Arista EOS Family").in_family("os")
         .detect do
           true
         end
+
       plat.name("arista_eos").title("Arista EOS").in_family("arista_eos")
         .detect do
           json_cmd("show version | json")
         end
 
-      # esx
       plat.family("esx").title("ESXi Family").in_family("os")
         .detect do
-          true if unix_uname_s =~ /vmkernel/i
+          unix_uname_s =~ /vmkernel/i
         end
+
       plat.name("vmkernel").in_family("esx")
         .detect do
           set_from_uname
         end
 
-      # cisco_ios family
       plat.family("cisco").title("Cisco Family").in_family("os")
         .detect do
-          !cisco_show_version.nil?
+          cisco_show_version
         end
 
       register_cisco("cisco_ios", "Cisco IOS", "cisco", :cisco_show_version, "ios")
       register_cisco("cisco_ios_xe", "Cisco IOS XE", "cisco", :cisco_show_version, "ios-xe")
       register_cisco("cisco_nexus", "Cisco Nexus", "cisco", :cisco_show_version, "nexus", "show version | include Processor")
 
-      # brocade family
       plat.family("brocade").title("Brocade Family").in_family("os")
         .detect do
-          !brocade_version.nil?
+          brocade_version
         end
 
       register_cisco("brocade_fos", "Brocade FOS", "brocade", :brocade_version, "fos")
@@ -517,18 +473,6 @@ module Train::Platforms::Detect::Specifications
         end
     end
 
-    def self.register_path_and_uname(name, title, family, path)
-      plat.name(name).title(title).in_family(family)
-        .detect do
-          if unix_file_contents(path) # TODO: this is unused and dumb.
-            # Because this is a rolling release distribution,
-            # use the kernel release, ex. 4.1.6-1-ARCH
-            @platform[:release] = unix_uname_r
-            true
-          end
-        end
-    end
-
     def self.register_lsb(name, title, family, regexp)
       register_lsb_or_content(name, title, family, nil, regexp)
     end
@@ -561,10 +505,30 @@ module Train::Platforms::Detect::Specifications
       plat.name(name).title(title).in_family(family)
         .detect do
           rel = unix_file_contents(path)
-          if (m = regexp.match(rel))
-            @platform[:release] = m[1]
+          if rel =~ regexp
+            @platform[:release] = $1
             true
           end
+        end
+    end
+
+    def self.register_path_and_uname(name, title, family, path)
+      plat.name(name).title(title).in_family(family)
+        .detect do
+          if unix_file_exist?(path)
+            # Because this is a rolling release distribution,
+            # use the kernel release, ex. 4.1.6-1-ARCH
+            # TODO: unix_uname_r.lines[0].chomp ? -- no tests for /etc/exherbo-release or /etc/arch-release
+            @platform[:release] = unix_uname_r
+            true
+          end
+        end
+    end
+
+    def self.register_path_regexp(name, title, family, path, regexp)
+      plat.name(name).title(title).in_family(family)
+        .detect do
+          regexp =~ unix_file_contents(path)
         end
     end
   end
