87f3d86b8ec02e7f74511ce6f26502c78a03d4b1
Vasu1105 <vasundhara.jagdale@chef.io>
2021-12-17 20:20:53 +0530

    Skipping Windows test for ssh and cisco_ios_connection which currently breaking when we run bundle exec rake on Windows. Individual running of the test files working correctly on the Windows.
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- test/unit/transports/ssh_test.rb
+++ test/unit/transports/ssh_test.rb
@@ -2,6 +2,11 @@ require "helper"
 require "train/transports/ssh"
 
 describe "ssh transport" do
+  before do
+    # This is to skip the test on windows as bundle exec rake is giving eror ArgumentError: non-absolute home
+    skip "not on windows" if windows?
+  end
+
   let(:cls) do
     plat = Train::Platforms.name("mock").in_family("linux")
     plat.add_platform_methods
@@ -264,6 +269,11 @@ describe "ssh transport" do
 end
 
 describe "ssh transport with bastion" do
+  before do
+    # This is to skip the test on windows as bundle exec rake is giving eror ArgumentError: non-absolute home
+    skip "not on windows" if windows?
+  end
+
   let(:cls) do
     plat = Train::Platforms.name("mock").in_family("linux")
     plat.add_platform_methods
@@ -299,6 +309,11 @@ describe "ssh transport with bastion" do
     end
 
     describe "opening a connection" do
+      before do
+        # This is to skip the test on windows as bundle exec rake is giving eror ArgumentError: non-absolute home
+        skip "not on windows" if windows?
+      end
+
       let(:ssh) { cls.new(conf) }
       let(:connection) { ssh.connection }
 
@@ -411,6 +426,11 @@ describe "ssh transport with bastion and proxy" do
 end
 
 describe "ssh transport ssh_config_file option" do
+  before do
+    # This is to skip the test on windows as bundle exec rake is giving eror ArgumentError: non-absolute home
+    skip "not on windows" if windows?
+  end
+
   let(:cls) do
     plat = Train::Platforms.name("mock").in_family("linux")
     plat.add_platform_methods
