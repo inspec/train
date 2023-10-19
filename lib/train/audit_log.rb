7ec5261158692052b012f29855ed26c9d2e359cb
Vasu1105 <vasundhara.jagdale@progress.com>
2023-10-19 11:54:36 +0530

    Fix typo
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>

--- lib/train/audit_log.rb
+++ lib/train/audit_log.rb
@@ -7,7 +7,7 @@ module Train
     def self.create(options = {})
       logger = Logger.new(options[:audit_log_location], options[:audit_log_frequency], options[:audit_log_size])
       logger.level = options[:level] || Logger::INFO
-      logger.progname = options[:audi_log_app_name]
+      logger.progname = options[:audit_log_app_name]
       logger.datetime_format = "%Y-%m-%d %H:%M:%S"
       logger.formatter = proc do |severity, datetime, progname, msg|
         {
